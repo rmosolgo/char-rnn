a2e01cdc4bb3eadbb32522970652ef0d8eb3b2b8
Robert Mosolgo <rdmosolgo@gmail.com>
2015-06-12 09:28:33 -0700

    update sample2

--- sample2.lua
+++ sample2.lua
@@ -1,5 +1,5 @@
-require "util.generate"
-io.write(generate(400))
-io.write("... " .. generate(40))
+local Generator = require "util.Generator2"
+io.write(Generator.generate(400))
+io.write("\n\n" .. Generator.generate(40))
 io.write('\n') io.flush()
 
